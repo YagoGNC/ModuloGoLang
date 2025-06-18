0dbd00014a5530d8530a97ac38fa442ec7359174
YagoGNC <ynigrocaserta@frba.utn.edu.ar>
2025-06-18 01:05:36 +0000

    actrualizo greetings

--- greetings/main.go
+++ greetings/main.go
@@ -14,6 +14,24 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	//Creamos un map con make para almacenar el mensaje y el nombre
+	messages := make(map[string]string)
+
+	//Recorremos nombre por nombre el slice
+	for _, name := range names {
+		//en caso de lograrlo, lo almacenamos en message
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		//agarramos el map, le pasamos como clave nombre y como valor el mensaje
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 func randomFormat() string {
 	formats := []string{
 		"¡Hola, %v! ¡Bienvenido!",
