4991e68bc1526c6e2965f38f52a4cffafb8e0779
YagoGNC <ynigrocaserta@frba.utn.edu.ar>
2025-06-18 00:48:01 +0000

    actualizo greetings

--- greetings/main.go
+++ greetings/main.go
@@ -3,12 +3,22 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Nombre vacio")
 	}
-	message := fmt.Sprintf("¡Bienvenido %v!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"¡Hola, %v! ¡Bienvenido!",
+		"¡Què bueo verte, %v!",
+		"¡Saludo, %v! ¡Encantado de conocerte!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
